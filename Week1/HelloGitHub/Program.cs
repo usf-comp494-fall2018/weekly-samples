335c817626e181a995ee50022915a3d424f50fb7
Scott Walton <swalton@stfrancis.edu>
2018-08-06 20:57:17 -0500

    Created Week 1 solution Created Hello Git Sample

---
+++ Week1/HelloGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello From GitHub!");
+        }
+    }
+}
