3995d736da41b0ee04fcf86e919b6c83fd2e74f8
Scott Walton <swalton@stfrancis.edu>
2018-09-30 22:47:03 -0500

    Removed code for demo purposes

--- Week6/PeopleLibrary/Employee.cs
+++ Week6/PeopleLibrary/Employee.cs
@@ -6,12 +6,5 @@ namespace PeopleLibrary
 {
     public class Employee : Person
     {
-        public void WriteToConsole()
-        {
-            Console.WriteLine($"{Name}'s age is {Age} and hire date was {HireDate:dd/MM/yyyy}");
-        }
-
-        public string EmloyeeCode { get; set; }
-        public DateTime HireDate { get; set; }
     }
 }
